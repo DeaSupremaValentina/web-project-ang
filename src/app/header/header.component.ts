384adc97a4dbd28670035edfcfc0c46f2e5220fb
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-01-07 20:02:16 +0100

    login e logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import auth from 'firebase/compat/app';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,23 +10,44 @@ import auth from 'firebase/compat/app';
 })
 export class HeaderComponent {
 isSidebarOpen: boolean = false;
+userLogged: boolean = false;
+
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 
   constructor(private afAuth: AngularFireAuth) { }
+  googleAuthProvider = new auth.auth.GoogleAuthProvider();
 
   loginWithGoogle() {
-    const googleAuthProvider = new auth.auth.GoogleAuthProvider();
-    this.afAuth.signInWithPopup(googleAuthProvider)
+      
+      this.afAuth.signInWithPopup(this.googleAuthProvider)
+      
       .then((result) => {
-        console.log(result.user);
+        console.log('result.user');
         // Puoi gestire l'utente autenticato qui
       })
       .catch((error) => {
         console.error(error);
       });
+      console.log('userloggato');
+      this.userLogged = true;
+  
+    
+  }
+
+  logout() {
+    
+    this.afAuth.signOut();
+    const user = this.afAuth.currentUser;
+    console.log('user disconnesso');
+    this.userLogged = false;
+
+  }
+
+  getUserLog() {
+    return this.userLogged;
   }
 
 }
