f6ee8cd2257dd18a37c067d0a154e0467a0d8593
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-01-10 17:50:40 +0100

    area personale fatta

--- src/app/area-personale/area-personale.component.ts
+++ src/app/area-personale/area-personale.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-area-personale',
   templateUrl: './area-personale.component.html',
-  styleUrl: './area-personale.component.css'
+  styleUrl: '../app.component.css'
 })
 export class AreaPersonaleComponent {
 
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: '../app.component.css'
 })
 export class FooterComponent {
 
