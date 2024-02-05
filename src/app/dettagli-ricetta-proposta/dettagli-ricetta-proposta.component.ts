e3e9b45f9d5524cf7415a1f0475e3e4950cf45ad
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-02-05 15:57:03 +0100

    ho copiato dettagli ricetta per le proposte

--- src/app/dettagli-ricetta-proposta/dettagli-ricetta-proposta.component.ts
+++ src/app/dettagli-ricetta-proposta/dettagli-ricetta-proposta.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RicetteServiceService } from '../ricette-service.service';
+import { Ricetta } from '../model/ricetta';
 
 @Component({
   selector: 'app-dettagli-ricetta-proposta',
   templateUrl: './dettagli-ricetta-proposta.component.html',
   styleUrl: './dettagli-ricetta-proposta.component.css'
 })
-export class DettagliRicettaPropostaComponent {
+export class DettagliRicettaPropostaComponent implements OnInit{
+
+  idRicetta: any;
+  ricetta: Ricetta | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private ricetteService: RicetteServiceService,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.idRicetta = params['id'];
+      this.loadRicettaDetails();
+    });
+  }
+
+  loadRicettaDetails() {
+    this.ricetteService.getRicettaByID(this.idRicetta).subscribe(
+      (data: Ricetta) => {
+        this.ricetta = data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
 }
