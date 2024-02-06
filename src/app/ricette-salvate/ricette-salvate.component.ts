403e7981d5285306d357a64df36fef5367b7a3d8
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-02-06 17:27:24 +0100

    aggiustato login e fatte mezze altre cose

--- src/app/ricette-salvate/ricette-salvate.component.ts
+++ src/app/ricette-salvate/ricette-salvate.component.ts
@@ -4,19 +4,19 @@ import { Ricetta } from '../model/ricetta';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-ricette-scritte-da-te',
-  templateUrl: './ricette-scritte-da-te.component.html',
-  styleUrl: './ricette-scritte-da-te.component.css'
+  selector: 'app-ricette-salvate',
+  templateUrl: './ricette-salvate.component.html',
+  styleUrl: '../app.component.css'
 })
-export class RicetteScritteDaTeComponent {
+export class RicetteSalvateComponent {
   ricette: any;
   constructor(private ricetteService: RicetteServiceService, private router: Router) {
-    this.ricetteService.dammiRicetteScritteDaUtente().subscribe((data) => {
+    this.ricetteService.dammiInfoRicette().subscribe((data) => {
       this.ricette = data;
     });
   }
   handleCardClick(ricetta: Ricetta) {
     this.router.navigate(['/ricette_id', ricetta.codice]);
   }
-
 }
+
