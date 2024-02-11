3a471b58526b9dcf4b8566c7e1ee07ceb3865292
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-02-11 11:41:10 +0100

    debug form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private auth: AuthService) { }
   title = 'web-project-ang';
   showSidebar = false;
 
